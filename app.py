174c9fd430ebdb4275450a40491bcf8e98da1802
Brian Ross <brian.ross@students.makeschool.com>
2020-11-10 00:41:12 -0500

    working on calendar api

--- app.py
+++ app.py
@@ -1,54 +1,84 @@
 """
-This code is used to display a guest list of who is coming to 
-my birthday party. This is a project to learn Flask
+Initialize your Flask app. This is what will run your server.
+
+Don't forget to install your dependencies from requirements.txt!
+This is a doc string! It's a special kind of comment that is expected
+in Python files. Usually, you use this at the top of your code and in
+every function & class to explain what the code does.
 """
-from flask import Flask, render_template, request
-from guests import Guest
-
-
-
-# This code initializes a basic flask application.
+from flask import Flask, request, render_template, url_for
+from guest import Guest
+from datetime import datetime, date
+from pprint import PrettyPrinter
+import requests
 
 app = Flask(__name__)
 
-#Variables for all the functions to use to pass into HTML
-my_name = 'Brian'
-date = 'April 10'
-time = '12:00 AM'
-#Making Guest List a global variable so all the functions can access it if they want to
-guest_friends = []
-# Homepage function to display my name
+# Define global variables (stored here for now)
+pp = PrettyPrinter(indent=4)
+guest_list = []
+
+today = date.today()
+month = today.strftime('%m')
+year = today.strftime("%Y")
+
 @app.route('/')
 def homepage():
     """Return template for home."""
-    return render_template('index.html', name=my_name)
+    return render_template('index.html')
 
-# Code for About Page
-@app.route("/about")
-def about():
+@app.route('/about')
+def about_page():
+    """Show user party information."""
+    # Sometimes, a cleaner way to pass variables to templates is to create a
+    # context dictionary, and then pass the data in by dictionary key
 
-    return render_template('about.html', time=time, date=date)
+    # TODO: your request code goes HERE
 
-# Display Guests who are coming
+    # TODO: access name, date, and description for all holidays this month. 
+    # HINT: you likely need to loop through the holidays in the data
+
+    holidays = []
+    # HINT: in holidays, we'll probably need a way to store key-value pairs.
+    # Think about the format we receive the response in
+    dates = # TODO: access data from your response
+    descriptions = # TODO: access data from your response
+
+    #  TODO: access the data we need from holidays, and make sure we can loop through this information in our template
+
+    context = {
+        "holidays": holidays,
+        "date": dates,
+        "description": descriptions
+    }
+
+    return render_template('about.html', **context)
+
+
 @app.route('/guests', methods=['GET', 'POST'])
-def guests():
-  if request.method == 'POST':
-    guest_name = request.form.get('name')
-    guest_email = request.form.get('email')
-    guest_plus_one = request.form.get("plus-one")
-    guest_phone = request.form.get('phone')
-    guest_costume = request.form.get('costume')
-    guest = Guest(guest_name, guest_email, guest_plus_one, guest_phone, guest_costume)
-    guest_friends.append(guest)
-    return render_template('guests.html', guests=guest_friends)
+def show_guests():
+    """
+    Show guests that have RSVP'd.
 
-# Display RSVP status
-@app.route("/rsvp", methods=['GET', 'POST'])
-def rsvp():
+    Add guests to RSVP list if method is POST.
+    """
+    if request.method == "GET":
+        return render_template("guests.html", guests=guest_list)
+    elif request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        plus_one = request.form.get("plus-one")
+        phone = request.form.get("phone")
+        costume = request.form.get("costume")
+        guest_list.append(Guest(name, email, plus_one, phone,costume))
+        return render_template("guests.html", guests=guest_list)
 
-    return render_template("rsvp.html", guest_friends=guest_friends)
-        
 
-# Runs code
+@app.route('/rsvp')
+def rsvp_guest():
+    """Show form for guests to RSVP for events."""
+    return render_template('rsvp.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ guest.py
@@ -0,0 +1,12 @@
+""" This is the Guest List"""
+
+class Guest:
+
+    def __init__(self, name, email, plus_one, phone, costume):
+        self.name = name
+        self.email = email
+        self.plus_one = plus_one
+        self.phone = phone
+        self.costume = costume
+
+
